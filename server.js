c0116f1d65047c0bd4f2492c17ad3be21619dd98
Hailey Bae <haejinbae@gmail.com>
2020-07-19 12:15:36 -0400

    pushing to heroku, places files to root of repository

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://googlehjb:Welcome123@ds039431.mlab.com:39431/heroku_x2s9n07r");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+if (process.env.NODE_ENV = "production") {
+  app.use(express.static("client/build"));
+}
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
